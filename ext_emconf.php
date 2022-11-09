d70741cede353806bb0114619cfe1ff89bf342c6
Benjamin Tammling <benjamin.tammling@sitegeist.de>
2022-11-09 15:04:59 +0100

    [TASK] Increase version

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,11 +9,11 @@ $EM_CONF['image_jack'] = [
     'state' => 'beta',
     'uploadfolder' => false,
     'clearCacheOnLoad' => true,
-    'version' => '0.9.1',
+    'version' => '0.9.2',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-12.9.99',
-            'php' => '7.4.0-8.9.99'
+            'php' => '7.4.0-8.1.99'
         ],
         'conflicts' => [
         ],
