5f71847a08a71537e49425991b37b9dd7f6bb143
Benjamin Tammling <benjamin.tammling@sitegeist.de>
2023-08-29 06:43:53 +0200

    [TASK] Add trait to extend getPublicUrl function

--- Classes/Xclass/AmazonS3Driver.php
+++ Classes/Xclass/AmazonS3Driver.php
@@ -2,9 +2,7 @@
 
 namespace Sitegeist\ImageJack\Xclass;
 
-class AmazonS3Driver extends \AUS\AusDriverAmazonS3\Driver\AmazonS3Driver {
-    public function getPublicUrl($identifier)
-    {
-        return parent::getPublicUrl($identifier);
-    }
+class AmazonS3Driver extends \AUS\AusDriverAmazonS3\Driver\AmazonS3Driver
+{
+    use DriverTrait;
 }
---
+++ Classes/Xclass/DriverTrait.php
@@ -0,0 +1,50 @@
+<?php
+namespace Sitegeist\ImageJack\Xclass;
+
+use Sitegeist\ImageJack\Templates\ConverterInterface;
+
+trait DriverTrait
+{
+    public function getPublicUrl($identifier)
+    {
+        if ($GLOBALS['TYPO3_REQUEST']->hasHeader('accept')) {
+            $acceptHeader = strtolower($GLOBALS['TYPO3_REQUEST']->getHeader('accept')[0]);
+            $templates = $this->sortTemplates($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['image_jack']['templates']);
+
+            /** @var ConverterInterface $template */
+            foreach ($templates as $template) {
+                if (in_array(ConverterInterface::class, class_implements($template))) {
+                    $targetMimeType = $template::getTargetMimeType();
+                    $targetFileExtension = $template::getTargetFileExtension();
+
+                    if (substr($identifier, -5) !== $targetFileExtension) {
+                        if (str_contains($acceptHeader, $targetMimeType)) {
+                            if ($this->fileExists($identifier . $targetFileExtension)) {
+                                $identifier .= $targetFileExtension;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return parent::getPublicUrl($identifier);
+    }
+
+    protected function sortTemplates($templates): array
+    {
+        usort($templates, function($a, $b) {
+            if (!method_exists($a, 'getPriority') && (!method_exists($b, 'getPriority'))) {
+                return 0;
+            } elseif (!method_exists($a, 'getPriority')) {
+                return -1;
+            } elseif (!method_exists($b, 'getPriority')) {
+                return 1;
+            } else {
+                return $a->getPriority() - $b->getPriority();
+            }
+        });
+
+        return $templates;
+    }
+}
