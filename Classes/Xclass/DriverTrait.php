6fa8819b5c0b0dffdc631fd5ed327679a48e08c9
Benjamin Tammling <benjamin.tammling@sitegeist.de>
2023-08-30 09:28:10 +0200

    [TASK] Refactor driver trait

--- Classes/Xclass/DriverTrait.php
+++ Classes/Xclass/DriverTrait.php
@@ -5,25 +5,19 @@ use Sitegeist\ImageJack\Templates\ConverterInterface;
 
 trait DriverTrait
 {
-    public function getPublicUrl($identifier)
+    public function getPublicUrl($identifier): ?string
     {
-        if ($GLOBALS['TYPO3_REQUEST']->hasHeader('accept')) {
-            $acceptHeader = strtolower($GLOBALS['TYPO3_REQUEST']->getHeader('accept')[0]);
-            $templates = $this->sortTemplates($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['image_jack']['templates']);
+        $fileInfo = $this->getFileInfoByIdentifier($identifier, ['mimetype']);
+        $templates = $this->sortAndFilterTemplates($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['image_jack']['templates']);
+        $mimeTypes = $this->extractMimetypesFromTemplates($templates);
 
-            /** @var ConverterInterface $template */
-            foreach ($templates as $template) {
-                if (in_array(ConverterInterface::class, class_implements($template))) {
-                    $targetMimeType = $template::getTargetMimeType();
-                    $targetFileExtension = $template::getTargetFileExtension();
+        /** @var ConverterInterface $template */
+        foreach ($templates as $template) {
+            $targetFileExtension = $template::getTargetFileExtension();
 
-                    if (substr($identifier, -5) !== $targetFileExtension) {
-                        if (str_contains($acceptHeader, $targetMimeType)) {
-                            if ($this->fileExists($identifier . $targetFileExtension)) {
-                                $identifier .= $targetFileExtension;
-                            }
-                        }
-                    }
+            if (!empty($fileInfo['mimetype']) && !in_array($fileInfo['mimetype'], $mimeTypes)) {
+                if ($this->fileExists($identifier . $targetFileExtension)) {
+                    $identifier .= $targetFileExtension;
                 }
             }
         }
@@ -31,20 +25,23 @@ trait DriverTrait
         return parent::getPublicUrl($identifier);
     }
 
-    protected function sortTemplates($templates): array
+    protected function sortAndFilterTemplates(array $templates): array
     {
+        $templates = array_filter($templates, function ($template) {
+            return in_array(ConverterInterface::class, class_implements($template));
+        });
+
         usort($templates, function ($a, $b) {
-            if (!method_exists($a, 'getPriority') && (!method_exists($b, 'getPriority'))) {
-                return 0;
-            } elseif (!method_exists($a, 'getPriority')) {
-                return -1;
-            } elseif (!method_exists($b, 'getPriority')) {
-                return 1;
-            } else {
-                return $a->getPriority() - $b->getPriority();
-            }
+            return $a::getPriority() - $b::getPriority();
         });
 
         return $templates;
+    }
+
+    protected function extractMimetypesFromTemplates(array $templates): array
+    {
+        return array_map(function ($template) {
+            return $template::getTargetMimeType();
+        }, $templates);
     }
 }
