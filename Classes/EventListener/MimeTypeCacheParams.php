9a9a8886d10234bd1250082224e328daebab61a5
Benjamin Tammling <benjamin.tammling@sitegeist.de>
2024-08-16 09:19:38 +0200

    [TASK] Only use hash event listener when fallback option is active

--- Classes/EventListener/MimeTypeCacheParams.php
+++ Classes/EventListener/MimeTypeCacheParams.php
@@ -17,20 +17,22 @@ final readonly class MimeTypeCacheParams
 {
     public function __invoke(BeforePageCacheIdentifierIsHashedEvent $event): void
     {
-        $request = $event->getRequest();
-        $params = $event->getPageCacheIdentifierParameters();
-        if ($request->hasHeader('accept')) {
-            $templates = array_filter($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['image_jack']['templates'], function ($template) {
-                return in_array(ConverterInterface::class, class_implements($template));
-            });
-            foreach ($templates as $template) {
-                $targetMimeType = $template::getTargetMimeType();
-                if (RequestHelper::checkForMimeTypeInAcceptHeader($request, $targetMimeType)) {
-                    $params['hashParameters']['accepts'][$targetMimeType] = true;
+        if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['image_jack']['useFallbackDriver'])) {
+            $request = $event->getRequest();
+            $params = $event->getPageCacheIdentifierParameters();
+            if ($request->hasHeader('accept')) {
+                $templates = array_filter($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['image_jack']['templates'], function ($template) {
+                    return in_array(ConverterInterface::class, class_implements($template));
+                });
+                foreach ($templates as $template) {
+                    $targetMimeType = $template::getTargetMimeType();
+                    if (RequestHelper::checkForMimeTypeInAcceptHeader($request, $targetMimeType)) {
+                        $params['hashParameters']['accepts'][$targetMimeType] = true;
+                    }
                 }
-            }
 
-            $event->setPageCacheIdentifierParameters($params);
+                $event->setPageCacheIdentifierParameters($params);
+            }
         }
     }
 }
